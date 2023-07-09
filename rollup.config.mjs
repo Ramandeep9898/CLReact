0df09fad1c0a20db4179aedd780b1a8c3dd04d43
Ramandeep <ramank6238299@gmail.com>
2023-07-09 18:31:33 +0530

    doc

---
+++ rollup.config.mjs
@@ -0,0 +1,40 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import image from "@rollup/plugin-image";
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.tsx",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      postcss({
+        plugins: [],
+        minimize: true,
+      }),
+      resolve(),
+      image(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
